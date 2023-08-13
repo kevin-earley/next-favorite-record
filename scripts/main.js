a045bf4ea8bb51cc4eb97fc94ae86865e4337083
Kevin Earley <kearley89@gmail.com>
2023-08-13 07:09:09 -0400

    revisions

--- scripts/main.js
+++ scripts/main.js
@@ -6,79 +6,76 @@ ui.linkNewResult.addEventListener('click', handleNewResultClick);
 ui.linkNewValues.addEventListener('click', handleNewValuesClick);
 
 async function handleSubmit(event) {
-  if (event.preventDefault) {
-    event.preventDefault();
-  }
+	if (event.preventDefault) {
+		event.preventDefault();
+	}
 
-  ui.removeAlert();
+	ui.removeAlert();
 
-  try {
-    if (ui.artistInput01.value === '' || ui.artistInput02.value === '') {
-      throw new Error('Please fill in both inputs.');
-    }
+	try {
+		if (ui.artistInput01.value === '' || ui.artistInput02.value === '') {
+			throw new Error('Please fill in both inputs.');
+		}
 
-    ui.hideForm();
-    ui.appendLoader();
+		ui.hideForm();
+		ui.appendLoader();
 
-    const similarArtists = await lastfm.getSimilarArtists([ui.artistInput01.value, ui.artistInput02.value]);
+		const similarArtists = await lastfm.getSimilarArtists([ui.artistInput01.value, ui.artistInput02.value]);
 
-    const filteredSimilarArtists = filterSimilarArtists(similarArtists);
+		const filteredSimilarArtists = filterSimilarArtists(similarArtists);
 
-    const randomSimilarArtist = filteredSimilarArtists[Math.floor(Math.random() * filteredSimilarArtists.length)];
+		const randomSimilarArtist = filteredSimilarArtists[Math.floor(Math.random() * filteredSimilarArtists.length)];
 
-    const topAlbums = await lastfm.getTopAlbums(randomSimilarArtist);
+		const topAlbums = await lastfm.getTopAlbums(randomSimilarArtist);
 
-    if (topAlbums.length < 50) {
-      throw new Error('Albums length error.');
-    }
+		if (topAlbums.length < 50) {
+			throw new Error('Albums length error.');
+		}
 
-    ui.removeLoader();
-    ui.showResults(filterTopAlbums(topAlbums));
-  } catch (error) {
-    if (error.message === 'Albums length error.') {
-      handleSubmit(event);
-    } else {
-      ui.removeLoader();
-      ui.showForm();
-      ui.appendAlert(error.message);
-    }
-  }
+		ui.removeLoader();
+		ui.showResults(filterTopAlbums(topAlbums));
+	} catch (error) {
+		if (error.message === 'Albums length error.') {
+			handleSubmit(event);
+		} else {
+			ui.removeLoader();
+			ui.showForm();
+			ui.appendAlert(error.message);
+		}
+	}
 }
 
 function filterSimilarArtists(similarArtists) {
-  const filteredSimilarArtists = similarArtists[0].filter((artist) => similarArtists[1].includes(artist));
+	const filteredSimilarArtists = similarArtists[0].filter((artist) => similarArtists[1].includes(artist));
 
-  if (filteredSimilarArtists.length === 0) {
-    throw new Error('No matches found.');
-  }
+	if (filteredSimilarArtists.length === 0) {
+		throw new Error('No matches found.');
+	}
 
-  return filteredSimilarArtists;
+	return filteredSimilarArtists;
 }
 
 function filterTopAlbums(topAlbums) {
-  const filteredTopAlbums = topAlbums
-    .filter((album) => removeCompilations(album.name.toLowerCase()))
-    .filter((album) => album.image[3]['#text'].length > 0);
+	const filteredTopAlbums = topAlbums.filter((album) => removeCompilations(album.name.toLowerCase())).filter((album) => album.image[3]['#text'].length > 0);
 
-  return filteredTopAlbums[0];
+	return filteredTopAlbums[0];
 }
 
 function removeCompilations(album) {
-  const compilationKeywords =
-    'best, collection, deluxe, disc, essential, greatest hits, hits, volume, edition, standard'.split(', ');
-  let flag = 0;
+	const compilationKeywords = 'best, collection, deluxe, disc, essential, greatest hits, hits, volume, edition, standard'.split(', ');
+	let flag = 0;
 
-  compilationKeywords.forEach((keyword) => (flag = flag + album.includes(keyword)));
+	compilationKeywords.forEach((keyword) => (flag = flag + album.includes(keyword)));
 
-  return flag === 0;
+	return flag === 0;
 }
 
 function handleNewResultClick() {
-  handleSubmit(event);
-  ui.hideResults();
+	handleSubmit(event);
+	ui.hideResults();
 }
 
 function handleNewValuesClick() {
-  ui.hideResults();
-  ui.showForm();
+	ui.hideResults();
+	ui.showForm();
 }
--- scripts/modules/ui.js
+++ scripts/modules/ui.js
@@ -1,101 +1,82 @@
 class UI {
-  constructor() {
-    this.card = document.querySelector('.card');
-    this.form = document.querySelector('.form');
-    this.artistInput01 = document.querySelector('#artist-input-01');
-    this.artistInput02 = document.querySelector('#artist-input-02');
-    this.btnSubmit = document.querySelector('.submit');
-    this.results = document.querySelector('.results');
-    this.resultsArtistLink = document.querySelector('.results-artist-link');
-    this.resultsTitle = document.querySelector('.results-title');
-    this.resultsImg = document.querySelector('.results-img');
-    this.btnApple = document.querySelector('.apple-music');
-    this.btnSpotify = document.querySelector('.spotify');
-    this.btnYouTube = document.querySelector('.youtube');
-    this.btnDiscogs = document.querySelector('.discogs');
-    this.linkNewResult = document.querySelector('.new-result');
-    this.linkNewValues = document.querySelector('.new-values');
-  }
+	constructor() {
+		this.card = document.querySelector('.card');
+		this.form = document.querySelector('.form');
+		this.artistInput01 = document.querySelector('#artist-input-01');
+		this.artistInput02 = document.querySelector('#artist-input-02');
+		this.btnSubmit = document.querySelector('.submit');
+		this.results = document.querySelector('.results');
+		this.resultsArtistLink = document.querySelector('.results-artist-link');
+		this.resultsTitle = document.querySelector('.results-title');
+		this.resultsImg = document.querySelector('.results-img');
+		this.btnYouTube = document.querySelector('.youtube');
+		this.btnDiscogs = document.querySelector('.discogs');
+		this.linkNewResult = document.querySelector('.new-result');
+		this.linkNewValues = document.querySelector('.new-values');
+	}
 
-  // Form Functions
-  showForm() {
-    this.form.style.display = 'block';
-  }
+	// Form Functions
+	showForm() {
+		this.form.style.display = 'block';
+	}
 
-  hideForm() {
-    this.form.style.display = 'none';
-  }
+	hideForm() {
+		this.form.style.display = 'none';
+	}
 
-  // Results Functions
-  showResults(similarArtistTopAlbum) {
-    this.results.style.display = 'block';
+	// Results Functions
+	showResults(similarArtistTopAlbum) {
+		this.results.style.display = 'block';
 
-    this.resultsArtistLink.textContent = similarArtistTopAlbum.artist.name;
+		this.resultsArtistLink.textContent = similarArtistTopAlbum.artist.name;
 
-    this.resultsArtistLink.href = `https://en.wikipedia.org/w/index.php?search=${similarArtistTopAlbum.artist.name
-      .toLowerCase()
-      .replace(/ /g, '+')}`;
+		this.resultsArtistLink.href = `https://en.wikipedia.org/w/index.php?search=${similarArtistTopAlbum.artist.name.toLowerCase().replace(/ /g, '+')}`;
 
-    this.resultsTitle.textContent = similarArtistTopAlbum.name;
+		this.resultsTitle.textContent = similarArtistTopAlbum.name;
 
-    this.resultsImg.src = similarArtistTopAlbum.image[3]['#text'];
+		this.resultsImg.src = similarArtistTopAlbum.image[3]['#text'];
 
-    this.btnApple.href = `https://music.apple.com/search?term=${similarArtistTopAlbum.artist.name
-      .toLowerCase()
-      .replace(/ /g, '+')}+${similarArtistTopAlbum.name
-      .toLowerCase().replace(/ /g, '+')}`;
+		this.btnYouTube.href = `https://www.youtube.com/results?search_query=${similarArtistTopAlbum.artist.name.toLowerCase().replace(/ /g, '+')}+${similarArtistTopAlbum.name.toLowerCase().replace(/ /g, '+')}`;
 
-    this.btnSpotify.href = `https://open.spotify.com/search/${similarArtistTopAlbum.artist.name
-      .toLowerCase()
-      .replace(/ /g, '%20')}%20${similarArtistTopAlbum.name
-        .toLowerCase().replace(/ /g, '%20')}`;
+		this.btnDiscogs.href = `https://www.discogs.com/search/?q=${similarArtistTopAlbum.artist.name.toLowerCase().replace(/ /g, '+')}+${similarArtistTopAlbum.name.toLowerCase()}`;
+	}
 
-    this.btnYouTube.href = `https://www.youtube.com/results?search_query=${similarArtistTopAlbum.artist.name
-      .toLowerCase()
-      .replace(/ /g, '+')}+${similarArtistTopAlbum.name.toLowerCase().replace(/ /g, '+')}`;
+	hideResults() {
+		this.results.style.display = 'none';
+		this.resultsArtistLink.textContent = '';
+		this.resultsArtistLink.href = '';
+		this.resultsTitle.textContent = '';
+		this.resultsImg.src = '';
+	}
 
-    this.btnDiscogs.href = `https://www.discogs.com/search/?q=${similarArtistTopAlbum.artist.name
-      .toLowerCase()
-      .replace(/ /g, '+')}+${similarArtistTopAlbum.name
-      .toLowerCase()}`;
-  }
+	// Loader Functions
+	appendLoader() {
+		if (document.querySelector('.loader') === null) {
+			const loaderImg = document.createElement('div');
+			loaderImg.classList.add('loader');
+			this.card.appendChild(loaderImg);
+		}
+	}
 
-  hideResults() {
-    this.results.style.display = 'none';
-    this.resultsArtistLink.textContent = '';
-    this.resultsArtistLink.href = '';
-    this.resultsTitle.textContent = '';
-    this.resultsImg.src = '';
-  }
+	removeLoader() {
+		if (document.querySelector('.loader') !== null) {
+			document.querySelector('.loader').remove();
+		}
+	}
 
-  // Loader Functions
-  appendLoader() {
-    if (document.querySelector('.loader') === null) {
-      const loaderImg = document.createElement('div');
-      loaderImg.classList.add('loader');
-      this.card.appendChild(loaderImg);
-    }
-  }
+	// Alert Functions
+	appendAlert(msg) {
+		const alertMsg = document.createElement('p');
+		alertMsg.classList.add('alert-msg');
+		alertMsg.textContent = msg;
+		this.card.appendChild(alertMsg);
+	}
 
-  removeLoader() {
-    if (document.querySelector('.loader') !== null) {
-      document.querySelector('.loader').remove();
-    }
-  }
-
-  // Alert Functions
-  appendAlert(msg) {
-    const alertMsg = document.createElement('p');
-    alertMsg.classList.add('alert-msg');
-    alertMsg.textContent = msg;
-    this.card.appendChild(alertMsg);
-  }
-
-  removeAlert() {
-    if (document.querySelector('.alert-msg') !== null) {
-      document.querySelector('.alert-msg').remove();
-    }
-  }
+	removeAlert() {
+		if (document.querySelector('.alert-msg') !== null) {
+			document.querySelector('.alert-msg').remove();
+		}
+	}
 }
 
 export const ui = new UI();
